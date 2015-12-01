cf6731f0267b56c2e9d52a4607c46c4bdac432c5
enricovianello <maverick17586@gmail.com>
2015-12-01 15:34:20 +0100

    Added srm PtP-Pd test

--- storm/base/common.py
+++ storm/base/common.py
@@ -4,71 +4,71 @@ import os
 import random
 
 class Utils:
-    
+
     def __init__(self, props):
         self.props = props
         self.is_init_srm_clients = False
         self.is_init_dav_clients = False
 
     def init_srm_clients(self):
-        
+
         if self.is_init_srm_clients:
             return
-        
+
         self.srm_clients = []
         frontends = [f.strip() for f in self.get_storm_fe_endpoint_list().split(',')]
         for f in frontends:
             client = SRMClientFactory.newSRMClient("https://%s" % f, self.get_proxy_file_path())
             self.srm_clients.append((f,client))
         self.is_init_srm_clients = True
-        
+
     def init_dav_clients(self):
-        
+
         if self.is_init_dav_clients:
             return
-        
+
         self.dav_clients = []
         davs = [dav.strip() for dav in self.get_storm_dav_endpoint_list().split(',')]
         for dav in davs:
             client = WebDAVClientFactory.newWebDAVClient("https://%s" % dav, self.get_proxy_file_path())
             self.dav_clients.append((dav,client))
         self.is_init_dav_clients = True
-        
+
     def get_proxy_file_path(self):
 
         uid = os.geteuid()
         if uid is "0":
             raise Exception("root user is not allowed!")
         return os.getenv('X509_USER_PROXY', "/tmp/x509up_u%s" % uid)
-    
+
     def get_storm_be_hostname(self):
 
         be_host = self.props["common.storm_be_hostname"]
         if be_host is None:
             raise Exception("Missed common.storm_be_hostname value")
         return be_host
-    
+
     def get_storm_fe_endpoint_list(self):
-        
+
         fe_list = self.props["common.storm_fe_endpoint_list"]
         if fe_list is None:
             raise Exception("Missed common.storm_fe_endpoint_list value")
         return fe_list
-    
+
     def get_storm_dav_endpoint_list(self):
-        
+
         dav_list = self.props["common.storm_dav_endpoint_list"]
         if dav_list is None:
             raise Exception("Missed common.storm_dav_endpoint_list value")
         return dav_list
 
     def get_srm_client(self):
-        
+
         self.init_srm_clients()
         return random.choice(self.srm_clients)
 
     def get_dav_client(self):
-        
+
         self.init_dav_clients()
         return random.choice(self.dav_clients)
 
@@ -101,3 +101,4 @@ class TestID():
     RM_TEST   = 108
     LS_TEST   = 109
     MIX_DAV   = 110
+    PTP_PD  = 111
---
+++ storm/ptp_pd/ptp_pd.py
@@ -0,0 +1,113 @@
+from common import *
+from srm import *
+from exceptions import Exception
+from jarray import array
+from java.io import FileInputStream
+from net.grinder.script import Test
+from net.grinder.script.Grinder import grinder
+import random
+import string
+import time
+import traceback
+import uuid
+
+error          = grinder.logger.error
+info           = grinder.logger.info
+debug          = grinder.logger.debug
+
+props          = grinder.properties
+
+utils           = Utils(grinder.properties)
+
+# Common variables:
+TEST_STORAGEAREA = props['common.test_storagearea']
+
+# Test specific variables
+TEST_DIRECTORY      = props['ptp_pd.test_directory']
+SLEEP_THRESHOLD     = int(props['ptp_pd.sleep_threshold'])
+SLEEP_TIME          = float(props['ptp_pd.sleep_time'])
+TEST_FILE_LIFETIME  = int(props['ptp_pd.pinLifetime_sec'])
+TEST_FILENAME       = "%s/%s/%s" % (TEST_STORAGEAREA, TEST_DIRECTORY, str(uuid.uuid4()))
+TEST_NUM_FILES      = int(props['ptp_pd.numfiles'])
+
+def get_base_dir():
+    return "%s/%s" % (TEST_STORAGEAREA, TEST_DIRECTORY)
+
+def do_prepare_to_put(client,surls):
+    (token, response) = srmPtP(client, surls, [], True, SLEEP_THRESHOLD, SLEEP_TIME)
+    return token
+
+def do_put_done(client, surls, token):
+    response = srmPd(client, surls, token)
+    log_result_file_status(response)
+
+def setup_thread_dir():
+    debug("Setup ptp_pd thread dir test ...")
+    endpoint, client = utils.get_srm_client()
+
+    base_dir = get_base_dir()
+    agent_dir = "%s/%s" % (base_dir, "a" + str(grinder.getAgentNumber()))
+    process_dir = "%s/%s" % (agent_dir, "p" + str(grinder.getProcessNumber()))
+    thread_dir = "%s/%s" % (process_dir, "t" + str(grinder.getThreadNumber()))
+
+    info("Creating remote test directory ... " + base_dir)
+    srmMkDir(client, get_surl(endpoint, base_dir))
+    info("Creating remote test directory ... " + agent_dir)
+    srmMkDir(client, get_surl(endpoint, agent_dir))
+    info("Creating remote test directory ... " + process_dir)
+    srmMkDir(client, get_surl(endpoint, process_dir))
+    info("Creating remote test directory ... " + thread_dir)
+    srmMkDir(client, get_surl(endpoint, thread_dir))
+
+    debug("setup ptp_pd completed successfully.")
+    return thread_dir
+
+def ptp_pd(client,surls):
+    debug("PtP on remote test files ... ")
+    token = do_prepare_to_put(client, surls)
+    grinder.sleep(TEST_FILE_LIFETIME*1000)
+    debug("Pd on remote test files ... " + token)
+    do_put_done(client, surls, token)
+
+def run_setup(endpoint, thread_dir):
+    debug("Setup run " + str(grinder.getRunNumber()) + " ...")
+
+    surls = []
+    for i in range(1, TEST_NUM_FILES + 1):
+        file_index = TEST_NUM_FILES * grinder.getRunNumber() + i
+        surl = get_surl(endpoint, "%s/file_%d" % (thread_dir, file_index))
+        surls.append(surl)
+        debug("appended: %s" % surl)
+
+    return surls
+
+
+class TestRunner:
+
+    def __init__(self):
+
+        self.thread_dir = setup_thread_dir()
+        info("Thread dir is: " + self.thread_dir)
+
+    def __call__(self):
+
+        try:
+
+            endpoint, client = utils.get_srm_client()
+
+            test = Test(TestID.PTP_PD, "StoRM PtP-PD test")
+            test.record(ptp_pd)
+
+            surls = run_setup(endpoint, self.thread_dir)
+            ptp_pd(client, surls)
+
+        except Exception, e:
+            error("Error executing ptp_pd: %s" % traceback.format_exc())
+            raise
+
+    def __del__(self):
+        info("srmRmDir %s ... " % self.thread_dir)
+        endpoint, client = utils.get_srm_client()
+        surl = get_surl(endpoint, self.thread_dir)
+    	response = client.srmRmdir(surl, True)
+    	info("srmRmDir %s response is %s %s" % (surl, response.returnStatus.statusCode, response.returnStatus.explanation))
